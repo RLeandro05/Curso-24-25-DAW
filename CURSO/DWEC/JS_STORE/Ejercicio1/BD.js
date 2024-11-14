b5f713d889881a69bf069a59a3cdab6119edbbee
RLeandro05 <XL3andr0_x>
2024-11-14 19:45:38 +0100

    Añadido código completo para poder crear la base de datos, además de añadir dos personas ininiciales a la tabla de la base de datos, y un método para eliminar dicha base de datos en caso de querer hacerlo

--- CURSO/DWEC/JS_STORE/Ejercicio1/BD.js
+++ CURSO/DWEC/JS_STORE/Ejercicio1/BD.js
@@ -14,4 +14,79 @@ function BD() {
 				estatura: { dataType: "number" },
 		}
 	};
+
+	//Crear nombre de la base de datos y añadir tanto el nombre como las tablas en la misma
+	let dbName ='Ejercicio1';
+	let dataBase = {
+		name: dbName,
+		tables: [tbPersonas]
+	}
+
+	//Función para crear la base de datos
+	const createDB = async (db)=>{
+		try {
+			const isDbCreated = await jsStoreCon.initDb(db);
+			if(isDbCreated === true){
+				console.log("db creada");
+				
+				insertarPersonas(); //Llamar a la función para insertar dos personas iniciales
+
+				console.log("Tablas iniciales creadas con éxito!");
+			}
+			else {
+				console.log("db abierto");
+			}
+		}
+		catch (ex) {
+			console.log(ex);
+			console.log(ex.message);
+			alert(ex.message);
+		}
+	}
+	
+	createDB(dataBase); //Crear la base de datos
+
+	//Función para insertar dos primeras personas
+	const insertarPersonas = async() => {
+		console.log("Entra en insertarPersonas");
+		
+		let personas = [
+			{
+				dni: "12345678A",
+				nombre: "Alfonso",
+				apellidos: "Domínguez Martín",
+				fecNac: new Date("1990-12-03"),
+				estatura: 176
+			},
+			{
+				dni: "12345678B",
+				nombre: "Laura",
+				apellidos: null,
+				fecNac: new Date("1992-03-12"),
+				estatura: 164
+			}
+		]
+
+		let insertCount = await jsStoreCon.insert({ //Insertar la lista de las personas a la tabla correspondiente
+			into: 'personas',
+			values: personas
+		});
+
+		//Asegurar que se insertaron
+		console.log("Insertadas: '"+insertCount+"' filas");
+		
+	}
+
+	//Función para eliminar la base de datos
+	/*const eliminarDB = async () => {
+        try {
+            await jsStoreCon.dropDb(dbName);
+            console.log("Base de datos eliminada con éxito");
+        } catch (ex) {
+            console.log("Error al eliminar la base de datos:", ex);
+        }
+    };
+
+    //Llamada a eliminarDB para probar
+    eliminarDB();*/
 }
