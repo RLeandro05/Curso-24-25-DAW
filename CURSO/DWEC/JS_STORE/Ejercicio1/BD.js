18b8168a85a095abbed9e4919c5982717a9d4f6c
RLeandro05 <XL3andr0_x>
2024-11-14 13:54:11 +0100

    Creada tabla de personas con sus respectivos campos en Ejercicio 1 de Js Store

---
+++ CURSO/DWEC/JS_STORE/Ejercicio1/BD.js
@@ -0,0 +1,17 @@
+function BD() {
+    //Crear una conexión para poder hacer una base de datos
+    let jsStoreCon = new JsStore.Connection(new Worker("jsstore/jsstore.worker.min.js"));
+
+    //Definir y crear la tabla de personas
+    let tbPersonas = {
+		name: 'personas', //Para indicar el nombre de la tabla
+		columns: { //Para crear los campos que tendrá la tabla
+				id: { primaryKey: true, autoIncrement: true },
+				dni: { notNull: true, dataType: "string"},
+				nombre: { notNull: true, dataType: "string"},
+				apellidos: { notNull: false, dataType: "string"},
+				fecNac: { notNull: true, dataType: "date_time"},
+				estatura: { dataType: "number" },
+		}
+	};
+}
