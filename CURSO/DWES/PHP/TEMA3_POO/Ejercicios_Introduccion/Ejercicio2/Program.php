baa9f612535c898c5f231bf5fe63b10d2308d5ca
RLeandro05 <XL3andr0_x>
2024-11-17 13:06:22 +0100

    Añadido identificador único y getters para acceder a la información de los atributo srápidamente en Ejercicio 2 de prueba POO

--- CURSO/DWES/PHP/TEMA3_POO/Ejercicios_Introduccion/Ejercicio2/Program.php
+++ CURSO/DWES/PHP/TEMA3_POO/Ejercicios_Introduccion/Ejercicio2/Program.php
@@ -1,9 +1,37 @@
 <?php
     class Producto {
         //Instanciar variables del producto
-        private $id;
-        private $nombre;
-        private $precio;
-        private $stock;
+        private static int $idIncrementa = 1;
+        private int $id;
+        private string $nombre;
+        private float $precio;
+        private int $stock;
+
+        public function __construct(string $nombre, float $precio, int $stock) {
+            $this->id = self::$idIncrementa++; //Primero asigna y luego incrementa
+            $this->nombre = $nombre;
+            $this->precio = $precio;
+            $this->stock = $stock;
+        }
+
+        //Getters para obtener rápidamente el valor
+        public function getId(){
+            return $this->id;
+        }
+
+        public function getNombre()
+        {
+                return $this->nombre;
+        }
+
+        public function getPrecio()
+        {
+                return $this->precio;
+        }
+
+        public function getStock()
+        {
+                return $this->stock;
+        }
     }
 ?>
