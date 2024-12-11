72d3ce5a7c209c4ab88a8b04a8631aa5c1b26872
RLeandro05 <lligpic577@g.educaand.es>
2024-12-11 10:30:32 +0100

    Modificaciones

--- CURSO/DWES/BasesDeDatos/Prueba/Acceso_BaseDeDatos.php
+++ CURSO/DWES/BasesDeDatos/Prueba/Acceso_BaseDeDatos.php
@@ -1,6 +1,19 @@
 <?php
-    $mysql = "mysql:host=localhost;dbname=dwes_prueba_m;charset=UTF8";
-    $user = "root";
-    $password = "";
-    $conexion = new PDO($mysql, $user, $password); 
+    try {
+        $mysql = "mysql:host=localhost;dbname=dwes_prueba_m;charset=UTF8";
+        $user = "root";
+        $password = "";
+        $opciones = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        $conexion = new PDO($mysql, $user, $password, $opciones); 
+
+        $version = $conexion->getAttribute(PDO::ATTR_SERVER_VERSION);
+
+        echo "<p>Conectado a la BBDD</p>";
+        echo "<p>Versión: ".$version."</p>";
+    } catch(PDOException $e) {
+        echo "<p>".$e->getMessage()."</p>";
+    } finally {
+        $conexion = null;
+    }
+    
 ?>
