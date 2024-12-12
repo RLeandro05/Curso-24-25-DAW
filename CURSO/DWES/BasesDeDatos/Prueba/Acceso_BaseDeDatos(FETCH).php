5e822d6faa86c7b5fa16407e16710877fbe65ded
RLeandro05 <lligpic577@g.educaand.es>
2024-12-12 09:44:05 +0100

    Adiciones Menores

---
+++ CURSO/DWES/BasesDeDatos/Prueba/Acceso_BaseDeDatos(FETCH).php
@@ -0,0 +1,21 @@
+<?php
+    try {
+        $mysql = "mysql:host=localhost;dbname=dwes_prueba_m;charset=UTF8";
+        $user = "root";
+        $password = "";
+        $opciones = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+        $conexion = new PDO($mysql, $user, $password, $opciones); 
+
+        echo "<p>Conectado a la BBDD</p>";
+
+        $resultado = $conexion->query("select * FROM mensajes");
+    while ($registro = $resultado->fetch(PDO::FETCH_OBJ)) {
+        echo "<p>Id: ".$registro->id."</p>";
+        echo "<p>Nombre: ".$registro->nombre."</p>";
+        echo "<p>Email: ".$registro->email."</p>";
+    }
+    } catch(PDOException $e) {
+        echo "<p>".$e->getMessage()."</p>";
+        exit();
+    }
+?>
