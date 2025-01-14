484917da64faf99056c9a7f2eee30c70e609bb87
RLeandro05 <lligpic577@g.educaand.es>
2025-01-14 14:48:13 +0100

    Realizado Ejercicio de prueba de Angular

--- CURSO/DWEC/ANGULAR/gHobits/src/app/app.component.ts
+++ CURSO/DWEC/ANGULAR/gHobits/src/app/app.component.ts
@@ -7,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'gHobits';
+  public lista: string[];
+  public gHobbit: string;
+  public gHobbitMod: string;
+  public idEdit: number = -1;
+  //private accion: Object;
+  private accion : {id: number, nombre:string, indice:number};
+
+  constructor(){
+    this.lista = ["Bilbo Bolson", "Sam Gmyi", "Frodo Bolson", "Pippin Pladin", "Mrry Brandigamo", "Rosita Goto"];
+    this.gHobbit= "";
+    this.gHobbitMod= "";
+    this.accion= {id:1, nombre:"Añadir", indice:-1};
+  }
+
+
+  eliminar(hobbit:string, i:number){
+    console.log("llega. hobbit: ",hobbit, "i: ",i);
+    if (confirm("¿Estás seguro de que deseas eliminar a "+hobbit+"?")) {
+      this.lista.splice(i, 1);
+    }
+  }
+
+  anade() {
+    console.log("Llega a anade, ", this.gHobbit);
+
+    if (this.gHobbit.trim() == "") {
+      alert("Está vacío");
+    } else {
+      this.lista.push(this.gHobbit);
+    }
+
+    this.gHobbit = "";
+  }
+
+  editar(hobbit:string, i:number) {
+    console.log("Llega a modificar, ", hobbit, i);
+
+    this.gHobbitMod = hobbit;
+    this.idEdit = i;
+  }
+
+  modificar() {
+    this.lista[this.idEdit] = this.gHobbitMod;
+  }
 }
