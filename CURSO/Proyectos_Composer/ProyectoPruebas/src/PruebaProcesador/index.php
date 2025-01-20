c2bac44ff591640e8a203ea00f2042f91e5126a1
RLeandro05 <lligpic577@g.educaand.es>
2025-01-20 09:20:50 +0100

    Update

---
+++ CURSO/Proyectos_Composer/ProyectoPruebas/src/PruebaProcesador/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once("../../vendor/autoload.php");
+
+    use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
+
+    // Crear un logger
+    $logger = new Logger('app');
+    $logger->pushHandler(new StreamHandler('logs/app.log', Logger::DEBUG));
+
+    // Crear un procesador para añadir el ID del usuario
+    $logger->pushProcessor(function ($record) {
+        $record['extra']['user_id'] = 123;  // Añadir información adicional
+        return $record;
+    });
+
+    // Generar un mensaje de prueba
+    $logger->info('Este es un mensaje informativo');
+    $logger->error('Este es un mensaje de error crítico');
+?>
