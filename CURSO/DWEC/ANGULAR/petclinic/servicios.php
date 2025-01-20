41abdea3d6e5bada00da9278c0e239d086c59645
RLeandro05 <lligpic577@g.educaand.es>
2025-01-20 14:51:28 +0100

    Update

--- CURSO/DWEC/ANGULAR/petclinic/servicios.php
+++ CURSO/DWEC/ANGULAR/petclinic/servicios.php
@@ -2,8 +2,9 @@
 
 header("Content-Type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET,POST,OPTIONS,POST,PUT');
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Credentials: true");
 
 /*
 header("Access-Control-Allow-Origin: *");
