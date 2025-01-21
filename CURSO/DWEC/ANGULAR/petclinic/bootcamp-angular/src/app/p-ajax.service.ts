90cf1f7327b8eaed0547cc5178f4d2c7a9f8aa99
RLeandro05 <lligpic577@g.educaand.es>
2025-01-21 12:53:28 +0100

    Update en ejercicio de Angular, añadiendo urls de clase y casa para el ejercicio

--- CURSO/DWEC/ANGULAR/petclinic/bootcamp-angular/src/app/p-ajax.service.ts
+++ CURSO/DWEC/ANGULAR/petclinic/bootcamp-angular/src/app/p-ajax.service.ts
@@ -7,7 +7,9 @@ import { HttpClient } from '@angular/common/http';
 
 export class PAjaxService {
 
-  private url: string = "http://localhost/Curso-24-25-DAW/DWEC/ANGULAR/petclinic/servicios.php";
+  private url: string = "http://localhost/Curso-24-25-DAW/CURSO/DWEC/ANGULAR/petclinic/servicios.php";
+  //Clase: "http://localhost/Curso-24-25-DAW/CURSO/DWEC/ANGULAR/petclinic/servicios.php";
+  //Casa: "http://localhost/Curso-24-25-DAW/DWEC/ANGULAR/petclinic/servicios.php";
 
   constructor(private http:HttpClient) { }
 
