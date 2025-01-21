cc702d6fb63667cbb77e6259bd69ed9de34bc23d
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-01-21 23:08:15 +0100

    Terminado ejercicio de Angular listadoPersonasModulos

--- CURSO/DWEC/ANGULAR/listadoPersonasModulos/src/app/modelos/persona.ts
+++ CURSO/DWEC/ANGULAR/listadoPersonasModulos/src/app/modelos/persona.ts
@@ -1,6 +1,6 @@
 export interface Persona {
-    id: number,
-    dni: string,
-    nombre: string,
-    apellidos: string,
+    ID: number,
+    DNI: string,
+    NOMBRE: string,
+    APELLIDOS: string,
 }
--- CURSO/DWEC/ANGULAR/listadoPersonasModulos/src/app/servicios/p-ajax.service.ts
+++ CURSO/DWEC/ANGULAR/listadoPersonasModulos/src/app/servicios/p-ajax.service.ts
@@ -7,8 +7,9 @@ import { Persona } from '../modelos/persona';
 })
 export class PAjaxService {
 
-  private url: string = "http://localhost/Curso-24-25-DAW/CURSO/DWEC/ANGULAR/listadoPersonasModulos/BACKENDS/servidor.php";
-  //http://localhost/Curso-24-25-DAW/CURSO/DWEC/ANGULAR/listadoPersonasModulos/BACKENDS/servidor.php
+  private url: string = "http://localhost/Curso-24-25-DAW/DWEC/ANGULAR/listadoPersonasModulos/BACKENDS/servidor.php";
+  //Clase: http://localhost/Curso-24-25-DAW/CURSO/DWEC/ANGULAR/listadoPersonasModulos/BACKENDS/servidor.php
+  //Casa: http://localhost/Curso-24-25-DAW/DWEC/ANGULAR/listadoPersonasModulos/BACKENDS/servidor.php
 
   constructor(private http:HttpClient) { }
 
