5ca08902c78913e4b59c449b2642361fb64a338c
RLeandro05 <lligpic577@g.educaand.es>
2025-01-23 09:24:30 +0100

    Update en ProyectoVideoClubMonolog

--- CURSO/DWES/PHP/TEMA3_POO/ProyectoVideoClubMonolog/util/LogFactory.php
+++ CURSO/DWES/PHP/TEMA3_POO/ProyectoVideoClubMonolog/util/LogFactory.php
@@ -1,20 +1,20 @@
 <?php
     namespace DawM\util;
 
-use Monolog\Handler\RotatingFileHandler;
-use Monolog\Logger;
-use Psr\Log\LoggerInterface;
+    use Monolog\Handler\RotatingFileHandler;
+    use Monolog\Logger;
+    use Psr\Log\LoggerInterface;
 
     class LogFactory {
         public static function getLogger(string $canal, string $ubicacion): LoggerInterface {
             //Crear un logger 
-            $miLog = new Logger('VideoclubLogger');
+            $miLog = new Logger($canal);
 
             //Agregar el RotatingFileHandler con nivel debug 
             $miLog->pushHandler(new RotatingFileHandler(__DIR__, 0, Logger::DEBUG));
 
             //Agregar el procesador de introspección y el manejador 
-            $miLog->pushHandler(new \Monolog\Handler\StreamHandler(__DIR__ . '/../logs/videoclub.log', Logger::DEBUG));
+            $miLog->pushHandler(new \Monolog\Handler\StreamHandler(__DIR__ . $ubicacion, Logger::DEBUG));
 
             return $miLog;
         }
