d29696676c4ee1e2a89587ace74f4481606420b4
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-01-29 21:26:03 +0100

    Update

--- CURSO/DWEC/ANGULAR/Para_Alumnado/examenAngular/src/app/components/lista-detalle/lista-detalle.component.ts
+++ CURSO/DWEC/ANGULAR/Para_Alumnado/examenAngular/src/app/components/lista-detalle/lista-detalle.component.ts
@@ -11,36 +11,47 @@ import { DetallesFactura } from '../../modules/detalles-factura';
   styleUrl: './lista-detalle.component.css'
 })
 export class ListaDetalleComponent {
+  //Lista de detalles para mostrar por pantalla después de cada petición de mostrar
   public detallesFactura: DetallesFactura[] = [];
 
+  //Variables donde se guarda del routerLink el id de factura y el número de la factura
   public idFactura: number = 0;
   public numeroFactura: string = "";
 
+  //Variables donde se van calculando el IVA y el total de los detalles de la factura
   public IVA: number = 0;
   public Total: number = 0;
 
+  //Objeto usado para iterar en los for de la lista de detalles
   public detalle: DetallesFactura = <DetallesFactura>{};
+  //Objeto para poder inicializar por defecto en el formulario
   public detalleFacturaForm: DetallesFactura = <DetallesFactura>{};
 
+  //Variables para calcular totales de IVA y de total
   public totalesIVA: number = 0;
   public totalesTotal: number = 0;
 
+  //Variable para ir alternando el texto del botón dependiendo de si se quiere añadir o editar
   public textoBoton: string = "Añadir línea de detalle";
 
   //Constructor que muestra el listado de los detalles de la factura seleccionada
   constructor(private activatedRoute: ActivatedRoute, private peticion: PAjaxService) {
+    //Se trae del routerLink id_factura y numero_factura
     this.idFactura = Number(this.activatedRoute.snapshot.params["id"]);
     this.numeroFactura = String(this.activatedRoute.snapshot.params["numero"]);
 
+    //Petición para listar los detalles de la factura seleccionada
     this.peticion.listarDetallesFactura(this.idFactura).subscribe(datos => {
       console.log("Id de Factura :>> ", this.idFactura);
       console.log("Numero de Factura :>> ", this.numeroFactura);
       this.detallesFactura = datos;
       console.log("detallesFactura :>> ", this.detallesFactura);
       
+      //Calcular ivas y totales
       this.calcularIvasyTotales();
     });
 
+    //Inicializar el objeto vacío para el formulario
     this.detalleFacturaForm = {
       id: -1,
       id_factura: 0,
@@ -53,6 +64,7 @@ export class ListaDetalleComponent {
     }
   }
 
+  //Método para calcular los ivas y totales cada vez se actualiza la tabla de los detalles
   calcularIvasyTotales() {
     this.totalesIVA = 0;
     this.totalesTotal = 0;
@@ -70,9 +82,12 @@ export class ListaDetalleComponent {
     }
   }
 
+  //Variable booleana para alternar la visualización del formulario
   public mostrarFormulario: boolean = false;
 
+  //Método que, según qué acción se realice, mostrar o esconder el formulario, y actualizar el objeto vacío
   toggleForm(accion: number, detalleTraido: DetallesFactura) {
+    //Si se decide añadir o terminar el añadido o modificación, cambiar el texto del botón y volver a vaciar el objeto
     if (accion == 1 || accion == 0) {
       this.textoBoton = "Añadir línea de detalle";
       this.detalleFacturaForm = {
@@ -89,7 +104,7 @@ export class ListaDetalleComponent {
 
       this.mostrarFormulario = !this.mostrarFormulario;
 
-    } else {
+    } else { //Si se decide modificar, cambiar el texto del botón y actualizar el objeto vacío con los datos traídos del HTML
       this.textoBoton = "Modificar línea de detalle";
       this.detalleFacturaForm = detalleTraido;
       //console.log("detalleFacturaForm :>> ", this.detalleFacturaForm);
@@ -99,7 +114,7 @@ export class ListaDetalleComponent {
       }
     }
 
-    console.log(this.mostrarFormulario);
+    console.log("mostrarFormulario :>> ", this.mostrarFormulario);
     
   }
 
@@ -109,18 +124,22 @@ export class ListaDetalleComponent {
     
     //console.log("idFactura :>> ", this.detalle["id_factura"]);
 
+    //Guardar en un nuevo campo el id_factura para poder realizar las peticiones
     detalleFacturaForm["id_factura"] = this.idFactura;
 
+    //Si el id de el detalle traído es '-1', es decir, que es un objeto vacío, estamos añadiendo un objeto, por lo que entrará aquí
     if (detalleFacturaForm.id == -1) {
       this.peticion.nuevoDetalle(detalleFacturaForm).subscribe(datos => {
         this.detallesFactura = datos;
   
+        //Calcular los nuevos ivas y totales
         this.calcularIvasyTotales();
       });
-    } else {
+    } else { //Si el id no es -1, significa que ha traído un objeto ya existente y modificado, por lo que entrará aquí para ser modificado
       this.peticion.editarDetalle(detalleFacturaForm).subscribe(datos => {
         this.detallesFactura = datos;
 
+        //Calcular los nuevos ivas y totales
         this.calcularIvasyTotales();
       })
     }
@@ -128,10 +147,12 @@ export class ListaDetalleComponent {
 
   //Método para borrar un detalle específico
   onClickBorrar(idDetalle: number, concepto: string) {
+    //Mandar aviso de borrado por si se arrepiente
     if (confirm("¿Estás seguro de que desea borrar '"+concepto+"'?")) {
       this.peticion.borrarDetalle(idDetalle, this.idFactura).subscribe(datos => {
         this.detallesFactura = datos;
 
+        //Calcular los nuevos ivas y totales
         this.calcularIvasyTotales();
       })
     }
