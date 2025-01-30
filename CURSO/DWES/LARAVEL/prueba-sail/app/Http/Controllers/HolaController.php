5fa0e5d2d9a501b13ac7464855cf2e92bf50927e
RLeandro05 <lligpic577@g.educaand.es>
2025-01-30 09:21:15 +0100

    Update

--- CURSO/DWES/LARAVEL/prueba-sail/app/Http/Controllers/HolaController.php
+++ CURSO/DWES/LARAVEL/prueba-sail/app/Http/Controllers/HolaController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HolaController extends Controller
 {
-    //
+    public function index() {
+        return "Hola, mundo";
+    }
+    public function show($nombre) {
+        //return "Hola, $nombre";
+        return view('saludo', ['nombre' => $nombre]);
+    }
 }
--- CURSO/DWES/LARAVEL/prueba-sail/routes/web.php
+++ CURSO/DWES/LARAVEL/prueba-sail/routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HolaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/hola", function() {
+/*Route::get("/hola", function() {
     return "Hola, mundo";
-});
+});*/
+
+Route::get('/hola', [HolaController::class, 'index']);
+Route::get('/hola/{nombre}', [HolaController::class, 'show']);
