39efe3b09d12c0d0f553ff77f435cd7de3ce02e9
RLeandro05 <lligpic577@g.educaand.es>
2025-02-11 14:48:35 +0100

    Update

--- CURSO/DWEC/ANGULAR/pet_clinic/src/app/components/pet-list/pet-list.component.ts
+++ CURSO/DWEC/ANGULAR/pet_clinic/src/app/components/pet-list/pet-list.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Pet } from '../../modules/pet';
 import { VisitListComponent } from "../visit-list/visit-list.component";
 import { PetService } from '../../services/pet.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pet-list',
@@ -13,7 +14,7 @@ export class PetListComponent {
   @Input() pets: Pet[] = [];
   @Output() petDeleted = new EventEmitter<number>();
 
-  constructor(private servicioPet: PetService) {}
+  constructor(private servicioPet: PetService, private route: Router) {}
 
   borrarPet(idPet: number, nombrePet: string) {
     if(confirm("¿Quieres eliminar a '"+nombrePet+"'?")) {
@@ -23,6 +24,14 @@ export class PetListComponent {
       },
       error => console.log("Error al borrar el pet :>> ", error));
     }
+  }
+
+  irAPetAdd(petAIrse: Pet) {
+    //console.log("idOwner :>> ", idOwner);
+    //console.log("pets :>> ", this.pets);
+    
+    
+    //this.route.navigate(["/pet-add", idOwner]);
   }
   
 }
