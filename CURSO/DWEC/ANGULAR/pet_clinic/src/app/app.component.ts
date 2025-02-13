f5b907e091ab01a089f84ca3035dbf5422892bfe
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-02-13 23:40:02 +0100

    Añadido menú superior funcional con nuevas funcionalidades por desarrollar

--- CURSO/DWEC/ANGULAR/pet_clinic/src/app/app.component.ts
+++ CURSO/DWEC/ANGULAR/pet_clinic/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,19 +16,39 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Mi Aplicación PrimeNG';
 
-  // Opciones para los dropdowns
   dropdown1Options = [
-    { label: 'Listar Owners', value: '1' },
+    { label: 'Owners', value: '1' },
     { label: 'Añadir Owner', value: '2' },
     { label: 'Opción 3', value: '3' }
   ];
 
   dropdown2Options = [
-    { label: 'Opción A', value: 'A' },
-    { label: 'Opción B', value: 'B' }
+    { label: 'Veterinarios', value: 'A' },
+    { label: 'Añadir Veterinario', value: 'B' }
   ];
 
   // Inicializamos las variables con valores válidos
   selectedOption1: string = '1'; // Valor por defecto para el primer dropdown
   selectedOption2: string = 'A'; // Valor por defecto para el segundo dropdown
+
+  constructor (private route: Router) {}
+
+  cambioDesplegableOwners(event: any) {
+    const seleccion = event.value;
+
+    switch (seleccion) {
+      case "1":
+        this.route.navigate(['/']);
+        break;
+      case "2":
+        this.route.navigate(['/form-owner', -1]);
+        break;
+      case "3":
+        this.route.navigate(['/']);
+    }
+  }
+
+  cambioDesplegableVeterinarios(event: any) {
+
+  }
 }
