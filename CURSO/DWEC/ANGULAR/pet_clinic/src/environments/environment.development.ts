67c073300000744334d69b4079da3c1b023b51b4
RLeandro05 <lligpic577@g.educaand.es>
2025-02-14 08:44:25 +0100

    Update

--- CURSO/DWEC/ANGULAR/pet_clinic/src/environments/environment.development.ts
+++ CURSO/DWEC/ANGULAR/pet_clinic/src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
-    API_URL: "http://localhost/Curso-24-25-DAW/DWEC/ANGULAR/pet_clinic/BACKENDS/servicios.php",
+    API_URL: "http://localhost/pet_clinicM/servicios.php",
     //Casa: "http://localhost/Curso-24-25-DAW/DWEC/ANGULAR/pet_clinic/BACKENDS/servicios.php"
-    //Clase: "http://localhost/Curso-24-25-DAW/CURSO/DWEC/ANGULAR/pet_clinic/BACKENDS/servicios.php"
 
     
 };
