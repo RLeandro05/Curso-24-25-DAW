4761356f5e81e494d48cf6e3ac69bf81b439239d
RLeandro05 <lligpic577@g.educaand.es>
2025-02-26 12:00:43 +0100

    Update

--- CURSO/DWEC/ANGULAR/loginJWT/src/app/modelos/reslogin.ts
+++
@@ -1,7 +0,0 @@
-export interface Reslogin {
-    login: boolean,
-    id: number,
-    usuario: string,
-    id_perfil: number,
-    perfil: string
-}
--- CURSO/DWEC/ANGULAR/loginJWT/src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    API_URL: "http://localhost/javascript/lista_personas_V1/servidor.php",
-};
